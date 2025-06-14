e4518df7ba0c72a9e8ab1da28e6b46b55305e14f
Pablo Álvarez <pablo.alvarezpanes949@comunidadunir.net>
2025-06-14 13:19:34 +0200

    funcion getAutorById en controlador

--- api_blog/src/controllers/autores.controller.js
+++ api_blog/src/controllers/autores.controller.js
@@ -1,5 +1,14 @@
-const getAll = (req, res) => {
-    res.send('Devuelve todos los autores');
+const Autor = require('../models/blog.model');
+
+const getAutorById = async (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id) || id <= 0) {
+        return res.status(400).json({ message: "El autor con este id no existe"});
+
+    }
+    const autor = await Autor.getAutorById(id);
+    res.json(autor);
+
 }
 
-module.exports = { getAll }
+module.exports = { getAutorById }
